3bf760d9e3f80111e46fcf6453372abecc91af0e
nutv99 <nutv99@gmail.com>
2021-02-23 18:32:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,12 +25,13 @@ $resultAr = explode("|",$result);
 
 
 if (trim($resultAr[0]) == "Fail") {
+  $resultAr[1] = 'scbeasysim://billpayment-anonymous/2dd86d7d-9476-4de1-8d20-e9d56b4b926e';
   pushMessage($resultAr[1],$access_token,$replyToken) ; 
   return;
 }  else {
   $ImageFileName = $result ; 
   pushImage($ImageFileName,$access_token,$replyToken);
-  $str = 'scbeasysim://billpayment-anonymous/2dd86d7d-9476-4de1-8d20-e9d56b4b926e';
+  
   pushMessage($str,$access_token,$replyToken) ; 
 }
 
