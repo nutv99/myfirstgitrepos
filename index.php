ff22901db2831e7e619ee577cdc502b6a67acc03
nutv99 <nutv99@gmail.com>
2022-02-17 13:29:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 $access_token = 'N0IzKf3n/tuu23eKxvUEkAY6Afzj8nu+lQYp+FyOAZXSVofsrCArcwRBOJKEbssASNnN5S35vUE5yiQ3dPcvlRqu9G0IVPHVxUHUHW63dUUUdxfcWpbZUj7iu8ImPFKK8LnAdy5wGDxvMhUD1A1fugdB04t89/1O/w1cDnyilFU='; 
 
-
+echo 'ssss';
 $sValue= getInputMessage() ;
 $MessageInput = $sValue[0];  
 $replyToken =  $sValue[1];  
